803603f20538f08b691af5b5880c1ab7a04a436c
caclm10 <lewinxander@gmail.com>
2023-10-26 21:44:39 +0700

    update action description

--- internal/action/action.go
+++ internal/action/action.go
@@ -1,14 +1,14 @@
 /*
 The action package functions more like a helper rather than a service that handles business logic.
-It does not use dependency injection, which allows the functions within this package to be used
+However, unlike typical helper functions, the action package has ties to models. For example,
+functions like Login and Logout are tied to the model.User.
+
+This package does not use dependency injection, which allows the functions within this package to be used
 across various services, facilitating repeated usage.
 
-Unlike services that handle business logic, the action package provides utility functions that can
-be used throughout the application. This makes it a versatile tool in the codebase.
-
-One such function within this package is Logout, which is used in the auth service's SignOut function
-and also in the user service's Delete function. This is an example of how functions in the
-action package can be used across services.
+Unlike services that handle business logic or helpers that are not tied to any models, the action
+package provides utility functions that are tied to specific models and can be used throughout
+the application. This makes it a versatile tool in the codebase.
 
 Example usage:
 
