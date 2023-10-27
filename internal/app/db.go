4ecd586aa64f29a8570dc7c7a4f03c185f186016
caclm10 <lewinxander@gmail.com>
2023-10-27 10:05:01 +0700

    add test db configuration

--- internal/app/db.go
+++ internal/app/db.go
@@ -7,26 +7,35 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB     *gorm.DB
+	TestDB *gorm.DB
+)
 
-var dbconfig = struct {
+type DBConfig struct {
 	Connection string
 	Host       string
 	Port       string
 	Database   string
 	Username   string
 	Password   string
-}{
-	Connection: Config.GetString("DB_CONNECTION"),
-	Host:       Config.GetString("DB_HOST"),
-	Port:       Config.GetString("DB_PORT"),
-	Database:   Config.GetString("DB_DATABASE"),
-	Username:   Config.GetString("DB_USERNAME"),
-	Password:   Config.GetString("DB_PASSWORD"),
+}
+
+func generateDSN(dbconfig DBConfig) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbconfig.Username, dbconfig.Password, dbconfig.Host, dbconfig.Port, dbconfig.Database)
 }
 
 func ConnectDB() {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbconfig.Username, dbconfig.Password, dbconfig.Host, dbconfig.Port, dbconfig.Database)
+	dbconfig := DBConfig{
+		Connection: Config.GetString("DB_CONNECTION"),
+		Host:       Config.GetString("DB_HOST"),
+		Port:       Config.GetString("DB_PORT"),
+		Database:   Config.GetString("DB_DATABASE"),
+		Username:   Config.GetString("DB_USERNAME"),
+		Password:   Config.GetString("DB_PASSWORD"),
+	}
+
+	dsn := generateDSN(dbconfig)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -34,4 +43,24 @@ func ConnectDB() {
 	}
 
 	DB = db
+}
+
+func ConnectTestDB() {
+	dbconfig := DBConfig{
+		Connection: Config.GetString("DB_TEST_CONNECTION"),
+		Host:       Config.GetString("DB_HOST"),
+		Port:       Config.GetString("DB_PORT"),
+		Database:   Config.GetString("DB_TEST_DATABASE"),
+		Username:   Config.GetString("DB_USERNAME"),
+		Password:   Config.GetString("DB_PASSWORD"),
+	}
+
+	dsn := generateDSN(dbconfig)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	TestDB = db
 }
